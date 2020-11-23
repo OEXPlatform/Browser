28af0b00218d13539274480cb8cc770e4cc27910
syslink <oursyslink@163.com>
2020-11-23 21:18:50 +0800

    add download link

--- src/pages/Download/Download.jsx
+++ src/pages/Download/Download.jsx
@@ -7,9 +7,13 @@ import mainBg from './images/download.png';
 import mainBgEn from './images/download-en.png';
 import iphone from './images/iphone.png';
 import android from './images/android.png';
+import erweima from './images/erweima.png';
 import { T, isChinese } from '../../utils/lang';
+import { Button, Balloon } from '@alifd/next';
 
 const Download = () => {
+  const iphoneTrigger = <a className='downloadItem' href='#'><img src={iphone} width='50'/><span>{T("iPhone下载")}</span></a>;
+  const androidTrigger = <a className='downloadItem' href='#'><img src={android} width='50'/><span>{T("Android下载")}</span></a>;
   return (
     <div className='layout'>
       <div className='dotBg'><img src={dot} width='305'/></div>
@@ -17,14 +21,21 @@ const Download = () => {
       <div className='mainContent'>
         <img src={isChinese() ? mainBg : mainBgEn} />
         <div className='download'>
-          <a className='downloadItem' href='#'>
+          <Balloon align="bl" alignEdge trigger={iphoneTrigger} closable={false}>
+            <img src={erweima} width='80'/>
+          </Balloon>
+          <Balloon align="bl" alignEdge trigger={androidTrigger} closable={false}>
+            <img src={erweima} width='80'/>
+          </Balloon>
+
+          {/* <a className='downloadItem' href='#'>
             <img src={iphone} width='50'/>
             <span>{T("iPhone下载")}</span>
           </a>
           <a className='downloadItem' href='#'>
             <img src={android} width='50'/>
             <span>{T("Android下载")}</span>
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
